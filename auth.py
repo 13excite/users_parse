4b927e3925a565a9999f7ea5411f59fead6345c0
Vladimir <originalblack@mail.ru>
2017-10-02 01:12:53 +0300

    added new auth method

--- auth.py
+++ auth.py
@@ -1,32 +1,17 @@
-import urllib2
-import sys
-import base64
-import urlparse
-import re
-#from bs4 import BeautifulSoup
+import requests
 
 
-theurl = "http://liga-znakomstv.ru/public/index.php"
+theurl = "http://liga-znakomstv.ru/public/profile.php?profile=4144"
 username = "test"
 passwd = "test1"
 
-req = urllib2.Request
-try:
-    handle = urllib2.urlopen(req)
-except IOError, e:
-    pass
-else:
-    # If we don't fail then the page isn't protected
-    print "This page isn't protected by authentication."
-    sys.exit(1)
-if not hasattr(e, 'code') or e.code != 401:
-    # we got an error - but not a 401 error
-    print "This page isn't protected by authentication."
-    print 'But we failed for another reason.'
-    sys.exit(1)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0',
+    'Cookie': 'blabla',
+      }
+r = requests.get(theurl)
+with open('test.html', 'w') as file:
+    file.write(r.text.encode('UTF-8'))
 
-authline = e.headers['www-authenticate']
-# this gets the www-authenticate line from the headers
-    # which has the authentication scheme and realm in it
 
 
