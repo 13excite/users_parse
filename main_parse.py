c76bae2ba9966d59ac81f720d654c9e2d6b43c88
Vladimir <originalblack@mail.ru>
2017-10-10 00:50:05 +0300

    add parse method in main script

--- main_parse.py
+++ main_parse.py
@@ -2,3 +2,33 @@ from bs4 import BeautifulSoup
 import re
 from auth import get_url
 
+URL = ''
+my_cookie = ''
+
+def re_parse(tag_list, re_pattern):
+    finish_list = []
+    p = re.compile(re_pattern)
+    for elm in tag_list:
+        if p.findall(elm):
+            finish_list.append(elm)
+    return finish_list
+
+def first_parse(html_doc):
+    """
+    find all 'a' tags in html doc
+     and return tag's list
+    """
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    finish_dict = dict()
+    a_tag_list = []
+
+    for tag in soup.find_all('a'):
+        a_tag_list.append(tag.get('href'))
+
+
+    return finish_dict
+
+
+if __name__ == '__main__':
+    html_doc = get_url(URL, my_cookie)
+
