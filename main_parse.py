fb5b3be54e7623e458a018ce283140d7ab2a2808
Vladimir <originalblack@mail.ru>
2017-12-22 00:45:12 +0300

    refactoring get_data_from_html methods

--- main_parse.py
+++ main_parse.py
@@ -3,8 +3,8 @@ import re
 from auth import get_url
 import json
 
-URL = 'http://liga-znakomstv.ru/public/profile.php?profile=4798'
-my_cookie = 'ddddd'
+URL = 'http://liga-znakomstv.ru/public/profile.php?profile=4805'
+my_cookie = 'fisdjfdjf'
 
 def re_parse(tag_list, re_pattern):
     finish_list = []
@@ -14,25 +14,34 @@ def re_parse(tag_list, re_pattern):
             finish_list.append(elm)
     return finish_list
 
-def get_main_info(html):
+def tags_getter(html, tag):
     soup = BeautifulSoup(html, 'html.parser')
     tag_list = []
-    for tag in soup.find_all('dd'):
+    for t in soup.find_all(tag):
         try:
-            tag_list.append(tag.text)
+            tag_list.append(t.text)
         except:
-            print 'Not find users info on page'
+            print 'Not find data in this tag'
     return tag_list
 
-def get_contacts(html_tags):
-    pass
 
-def generic_html_table(name, contacts, age, sex):
+def get_contacts(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    my_link = []
+    for link in soup.find_all('a'):
+        my_link.append(link.get('href'))
+    return my_link
+
+def a_tags_parser(list_urls):
     pass
 
 
 if __name__ == '__main__':
-    html_doc = get_main_info(get_url(URL, my_cookie))
-    for elm in html_doc:
+    html_doc = get_url(URL, my_cookie)
+
+    user_info_tags = tags_getter(html_doc, 'dd')
+    contacs = get_contacts(html_doc)
+    print contacs
+    for elm in user_info_tags:
         print "<p>%s</p>" % elm
 
